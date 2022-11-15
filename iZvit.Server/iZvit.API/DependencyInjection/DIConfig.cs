593cc63def9d15e2c192d07cb0fea260c89a7583
OlehMatsevych <oleg.macevich.pz.2019.nulp.com@gmail.com>
2022-11-15 17:30:53 +0200

    feat: logic

--- iZvit.Server/iZvit.API/DependencyInjection/DIConfig.cs
+++ iZvit.Server/iZvit.API/DependencyInjection/DIConfig.cs
@@ -1,4 +1,9 @@
-﻿using iZvit.DataAccess.Persistence;
+﻿using iZvit.Application.MappingProfiles;
+using iZvit.Application.Services;
+using iZvit.Application.Services.Contracts;
+using iZvit.DataAccess.Persistence;
+using iZvit.DataAccess.Repositories;
+using iZvit.DataAccess.Repositories.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -8,11 +13,15 @@ namespace iZvit.API.DependencyInjection
     {
         public static void AddServices(this IServiceCollection services)
         {
-            //services.AddScoped<IApartmentService, ApartmentService>();
+            services.AddScoped<IReportService, ReportService>();
+        }
+        public static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(ReportProfile).Assembly);
         }
         public static void AddRepositories(this IServiceCollection services)
         {
-            //services.AddScoped<IApartmentRepository, ApartmentRepository>();
+            services.AddScoped<IReportRepository, ReportRepository>();
         }
 
         public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
@@ -21,38 +30,6 @@ namespace iZvit.API.DependencyInjection
             services.AddDbContext<Context>(options =>
                 options.UseSqlServer(databaseConfig.ConnectionString,
                 opt => opt.MigrationsAssembly(typeof(Context).Assembly.FullName)));
-        }
-        public static void AddSwagger(this IServiceCollection services)
-        {
-            services.AddSwaggerGen(s =>
-            {
-                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer YOUR_TOKEN')",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                s.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oath2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-            });
         }
     }
 }
---
+++ iZvit.Server/iZvit.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using iZvit.Core.Entities;
+using iZvit.DataAccess.Persistence;
+using iZvit.DataAccess.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace iZvit.DataAccess.Repositories
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private Context _context;
+        public BaseRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public Task AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().AddAsync(entity);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            return _context.SaveChangesAsync();
+        }
+        public Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() =>
+            await _context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity> GetByIdAsync<T>(T id)
+        {
+            if (id is Guid || id is int || id is string)
+            {
+                return await _context.Set<TEntity>().FindAsync(id);
+            }
+            throw new ArgumentException();
+        }
+        public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> predicate) =>
+             _context.Set<TEntity>().Where(predicate).AsQueryable();
+
+        public IQueryable<TEntity> GetAll(params Expression<Func<TEntity, object>>[] includes)
+        {
+            var query = _context.Set<TEntity>().AsQueryable();
+            return includes.Aggregate(query, (q, w) => q.Include(w));
+        }
+    }
+}
+
