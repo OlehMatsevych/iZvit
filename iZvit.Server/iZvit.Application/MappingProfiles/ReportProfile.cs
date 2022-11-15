100e463aa345d0a780865705210c5d99c76a1f43
OlehMatsevych <oleg.macevich.pz.2019.nulp.com@gmail.com>
2022-11-15 20:16:05 +0200

    feat:dfdfgdfgdfg

---
+++ iZvit.Server/iZvit.Application/MappingProfiles/ReportProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using iZvit.Application.Models;
+using iZvit.Core.Entities;
+
+namespace iZvit.Application.MappingProfiles
+{
+    public class ReportProfile: Profile
+    {
+        public ReportProfile()
+        {
+            CreateMap<Report, ReportModel>();
+            CreateMap<ReportModel, Report>();
+        }
+    }
+}
---
+++ iZvit.Server/iZvit.Application/Responses/OperationStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace iZvit.Application.Responses
+{
+    public class OperationStatus
+    {
+        public bool IsSuccesed { get; set; }
+        public string? Message { get; set; }
+    }
+}
